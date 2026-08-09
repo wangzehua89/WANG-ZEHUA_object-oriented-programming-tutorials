e75a3790389470881e430bc3484d15c8a084cf52
wangzehua89 <154318240@qq.com>
2026-08-09 13:41:17 +0000

    week6

---
+++ week6/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        lecturer lec = new lecturer("L100", "Dr Ahmad", "Faculty of Information Technology", "Java Programming");
+        lec.displayInfo();
+        lec.displaySubject();
+    }
+}
---
+++ week6/employee.java
@@ -0,0 +1,16 @@
+public class employee {
+    protected String id;
+    protected String name;
+    protected String department;
+
+    public employee(String id, String name, String department){
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public void displayInfo(){
+        System.out.println("Employee ID : " + id);
+        System.out.println("Name        : " + name);
+    }
+}
