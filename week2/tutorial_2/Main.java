758318a802d898ef753be92fa5ac3df944472cd3
wangzehua89 <154318240@qq.com>
2026-08-09 13:30:03 +0000

    week2

---
+++ week2/tutorial_2/Main.java
@@ -0,0 +1,14 @@
+package week2.tutorial_2;
+
+public class Main {
+    public static void main(String[] args) {
+        // coding starts here;
+
+        Student s1 = new Student("Tom", 20, 3.7);
+
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
+
+    }
+}
---
+++ week2/tutorial_2/Student.java
@@ -0,0 +1,27 @@
+package week2.tutorial_2;
+
+class Student {
+    String name;
+    int age;
+    double gpa;
+
+    Student(String studentName, int studentAge, double studentGpa) {
+        name = studentName;
+        age = studentAge;
+        gpa = studentGpa;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+    }
+
+    public void study() {
+        System.out.println(name + " is studying");
+    }
+
+    public void takeExam() {
+        System.out.println(name + " is taking an exam");
+    }
+}
